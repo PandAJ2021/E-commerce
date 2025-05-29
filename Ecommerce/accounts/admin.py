97c36d356c3eea2091db3c5db8c6c0c8367c19f9
AliJalili <pandaj200021@gmail.com>
2025-05-29 18:53:14 +0330

    Customize Django admin for custom User model

--- Ecommerce/accounts/admin.py
+++ Ecommerce/accounts/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from .forms import CreateUserForm, ChangeUserForm
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    form = ChangeUserForm
+    add_form = CreateUserForm
+
+    list_display = ('phone', 'email', 'name', 'is_admin')
+    list_filter = ('is_admin',)
+
+    fieldsets = (
+        (None, {'fields':('phone', 'email', 'password')}),
+        ('Personal info', {'fields':('name',)}),
+        ('Permissions', {'fields':('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        
+        (None, {'classes': ('wide',),
+                'fields':('phone', 'email', 'password1', 'password2')
+                }),
+    )
+
+    search_fields = ('phone', 'email')
+    ordering = ('phone',)
+    filter_horizontal = ()
+
+
+admin.site.unregister(Group)
+admin.site.register(User,UserAdmin)
